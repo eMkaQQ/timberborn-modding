0622b973998f754fbda0ca5639a12ecbca2f3e2b
eMka <nodramakky@gmail.com>
2024-09-25 08:53:59 +0200

    Fixed ModSettingsExamples crash on GameScene

--- Assets/Mods/ModSettingsExamples/Scripts/ModSettingsConfigurator.cs
+++ Assets/Mods/ModSettingsExamples/Scripts/ModSettingsConfigurator.cs
@@ -10,6 +10,14 @@ namespace ModSettingsExamples {
       containerDefinition.Bind<SimpleSettingsExample>().AsSingleton();
       containerDefinition.Bind<SettingsLogger>().AsSingleton();
       containerDefinition.Bind<FileStoredSettingsExample>().AsSingleton();
+    }
+
+  }
+  
+  [Context("MainMenu")]
+  internal class MainMenuConfigurator : IConfigurator {
+
+    public void Configure(IContainerDefinition containerDefinition) {
       containerDefinition.Bind<ValueChangeListenerExample>().AsSingleton();
     }
 
